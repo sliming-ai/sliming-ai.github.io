62216f5c1532d40ee9bc63d313aac9a7b0f91082
t <pvtien96@gmail.com>
2024-11-07 11:21:35 +0100

    Fix color

--- update.py
+++ update.py
@@ -7,6 +7,7 @@ plt.rcParams["font.family"] = "DejaVu Sans"
 
 # Constants
 SUBMISSION_START = datetime.datetime(2024, 10, 31)
+SUBMISSION_TO_ACCEPTANCE = 195  # average time for acceptance
 mouse_size = 0.0
 
 
@@ -20,7 +21,8 @@ def calculate_time_progress():
 
 def generate_progress_badge(time_spent):
     # Get color from the RdYlGn colormap
-    colors = plt.cm.RdYlGn(time_spent)
+    progress = time_spent / SUBMISSION_TO_ACCEPTANCE
+    colors = plt.cm.RdYlGn(1-progress)
 
     # Generate Markdown text for the badge
     badge_text = f"![Days since submission](https://img.shields.io/badge/Days_since_submission-{time_spent}-{mcolors.to_hex(colors)[1:]}?style=flat-square)"
